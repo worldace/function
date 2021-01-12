83b457634cf52093542e948379047359dea38f78
tokaiknight <tokaiknight@yahoo.co.jp>
2021-01-12 16:20:09 +0900

    no message

--- function.php
+++ function.php
@@ -1865,12 +1865,24 @@ class template{
     function __toString(){
         extract((array)$this);
         ob_start();
-        eval('?>'.$this->compile());
+        include $this->cache();
         return ob_get_clean();
     }
 
+    function cache(){
+        $cache = "$this->tempLate.php";
+        $mtime = filemtime($this->tempLate);
+
+        if($mtime !== @filemtime($cache)){
+            file_put_contents($cache, $this->compile(), LOCK_EX);
+            touch($cache, $mtime);
+        }
+        return $cache;
+    }
+
     function compile(){
-        return preg_replace_callback(['/(\{\{)(.+?)\}\}/', '/<(if|elseif|foreach|include) code="(.+?)">/s', '/<(\/)(if|foreach)>/', '/<(else)>/'], 'self::callback', $this->tempLate);
+        $re = ['/(\{\{)(.+?)\}\}/s', '/<(if|elseif|foreach|include) code="(.+?)">/s', '/<(\/)(if|foreach)>/', '/<(else)>/'];
+        return preg_replace_callback($re, 'self::callback', file_get_contents($this->tempLate));
     }
 
     private static function callback($m){
